f7285911c5a103031cde2043d118f305d755a858
ng-lee <nhd5607@gmail.com>
2025-06-23 03:07:20 +0900

    fix: member request,entity 필드 추가 및 컬럼명 수정

--- src/main/java/com/modoo/domain/member/entity/Member.java
+++ src/main/java/com/modoo/domain/member/entity/Member.java
@@ -2,49 +2,72 @@ package com.modoo.domain.member.entity;
 
 import com.modoo.domain.member.dto.request.MemberRequest;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "member")
 @Getter
 @Builder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long cd;
+    private Long memberCd;
 
     @Column(length = 20)
-    private String id;
+    @Comment(value = "회원 아이디")
+    private String memberId;
 
-    @Column(length = 255)
+    @Comment(value = "비밀번호")
     private String password;
 
     @Column(length = 50, nullable = false)
+    @Comment(value = "이름")
     private String name;
 
     @Column(length = 20, nullable = false)
+    @Comment(value = "휴대폰 번호 (010-1234-4567)")
     private String phone;
 
     @Column(length = 10, nullable = false)
+    @Comment(value = "이름")
     private Integer categoryCd;
 
     @Column(length = 10, nullable = false)
+    @Comment(value = "지역 코드")
     private Integer regionCd;
+
+    @Comment(value = "프로필 사진")
+    private String profile;
+
+    @Comment(value = "refresh token")
+    private String refreshToken;
+
+    @Comment(value = "refresh token 만료 시간")
+    private LocalDateTime refreshTokenExpirationTime;
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public void updateRefreshTokenExpirationTime(LocalDateTime refreshTokenExpirationTime) {
+        this.refreshTokenExpirationTime = refreshTokenExpirationTime;
+    }
 
     public static Member of(MemberRequest request) {
         return Member.builder()
-                .id(request.getId())
+                .memberId(request.getMemberId())
                 .password(request.getPassword())
                 .name(request.getName())
                 .phone(request.getPhone())
                 .categoryCd(request.getCategoryCd())
                 .regionCd(request.getRegionCd())
+                .profile(request.getProfile())
                 .build();
     }
 
