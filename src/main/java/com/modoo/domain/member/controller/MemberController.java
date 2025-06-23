7f24fbaffea95bf190dea9b6095267a83a813382
ng-lee <nhd5607@gmail.com>
2025-06-23 22:12:18 +0900

    feature: 마이페이지 이동 api 추가

--- src/main/java/com/modoo/domain/member/controller/MemberController.java
+++ src/main/java/com/modoo/domain/member/controller/MemberController.java
@@ -1,11 +1,15 @@
 package com.modoo.domain.member.controller;
 
+import com.modoo.domain.member.dto.auth.MemberAuth;
 import com.modoo.domain.member.dto.request.MemberRequest;
 import com.modoo.domain.member.dto.response.MemberResponse;
 import com.modoo.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -20,6 +24,12 @@ public class MemberController {
     public void create(@RequestBody MemberRequest request) {
        MemberResponse memberResponse = memberService.create(request);
        log.info("MemberResponse : {}", memberResponse);
+    }
+
+    @GetMapping("/myPage")
+    public String toMyPage(@AuthenticationPrincipal MemberAuth memberAuth, Model model) {
+        model.addAttribute("member", memberAuth);
+        return "/member/myPage";
     }
 
 }
