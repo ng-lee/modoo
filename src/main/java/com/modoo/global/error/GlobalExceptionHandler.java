e9d752d409d52d57df295d365c791bab420e52c4
ng-lee <nhd5607@gmail.com>
2025-06-27 00:49:03 +0900

    feature: 비동기 요청 실패 시 에러 페이지로 이동하기 위한 exception handler 추가

---
+++ src/main/java/com/modoo/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.modoo.global.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity exceptionHandler(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+}
