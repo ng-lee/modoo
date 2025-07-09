f682cf2c1eaf8d9373f653c3ca55bf7faf8699a8
ng-lee <nhd5607@gmail.com>
2025-07-09 18:17:26 +0900

    Revert "feature: OAuth2 사용자 정보 인터페이스 및 팩토리 구현"
    This reverts commit 13ebc1250f7703ff7c5a57f71471edbe22883f2b.

--- src/main/java/com/modoo/domain/member/service/userinfo/OAuth2UserInfo.java
+++
@@ -1,8 +0,0 @@
-package com.modoo.domain.member.service.userinfo;
-
-public interface OAuth2UserInfo {
-    String getProvider();
-    String getProviderId();
-    String getEmail();
-    String getName();
-}
