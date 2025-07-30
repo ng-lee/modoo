f826269f04c837d760de29eda2bee3b7c061897c
ng-lee <nhd5607@gmail.com>
2025-07-30 17:18:26 +0900

    fix: 메인 지역 정보 초기화 메소드 추가

--- src/main/java/com/modoo/domain/metadata/repository/RegionDongRepository.java
+++ src/main/java/com/modoo/domain/metadata/repository/RegionDongRepository.java
@@ -2,9 +2,19 @@ package com.modoo.domain.metadata.repository;
 
 import com.modoo.domain.metadata.entity.RegionDong;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RegionDongRepository extends JpaRepository<RegionDong, Long> {
     List<RegionDong> findBySigunguCd_sigunguCd(Long sigunguCd);
+
+    @Query(value = """
+            select rd
+            from RegionDong rd
+            join fetch rd.sigunguCd
+            where rd.dongCd = :dongCd            
+            """
+    )
+    RegionDong getInitialRegionData(Long dongCd);
 }
